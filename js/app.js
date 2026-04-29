1377bf352bfd711668bb9ee3966e32a899e7c5c4
jfcnetto <jfcnetto@gmail.com>
2026-04-29 15:35:34 -0300

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -1,1 +1,34 @@
+let etapa = 0;
+let pontos = 0;
 
+function render(){
+document.getElementById("pergunta").innerText = historia[etapa].pergunta;
+
+let html="";
+historia[etapa].opcoes.forEach((op,i)=>{
+html += `<button onclick="responder(${i})">${op}</button>`;
+});
+
+document.getElementById("opcoes").innerHTML = html;
+}
+
+function responder(i){
+if(i === historia[etapa].correta){
+pontos += 10;
+document.getElementById("feedback").innerHTML = "✅ Correto";
+
+etapa++;
+
+if(etapa < historia.length){
+setTimeout(render,500);
+}else{
+document.getElementById("pergunta").innerText = "Fim!";
+}
+}else{
+document.getElementById("feedback").innerHTML = "❌ Tente novamente";
+}
+
+document.getElementById("pontos").innerText = pontos;
+}
+
+render();
